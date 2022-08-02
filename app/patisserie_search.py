d682cbfc00a65b58dabc6e8c08361200b074b3b8
shinji-kodama2 <kodama@aniverse.co.jp>
2022-08-03 00:51:02 +0900

    add_individual_informations

--- app/patisserie_search.py
+++ app/patisserie_search.py
@@ -4,16 +4,39 @@ from bs4 import BeautifulSoup
 import math
 import time
 
-def test():
+# params: url
+# return: soup
+def get_parsed_html(url):
+  url = url.strip()
+  try:
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'html.parser')
+  except:
+    print('error: ' + url)
+    return False
+
+# params: csv用のlist
+# return: void
+def create_csv(f_name, list):
+  with open(f_name, 'w', encoding='utf_8_sig') as f:
+    for l in list:
+      print(l)
+      f.write(','.join(l) + '\n')
+  f.close()
+
+# 食べログの検索結果一覧からURLを取得する
+# params: void
+# return: void
+def get_patisserie_list():
   csv = [['shop_name', 'url', 'place', 'types', 'star', 'price_dinner', 'price_lunch', 'holiday']]
+
   base_url = 'https://tabelog.com/tokyo/rstLst/'
   page = 1
   query = '/?sw=パティスリー'
 
   url = base_url + str(page) + query
 
-  html = requests.get(url).text
-  soup = BeautifulSoup(html, 'html.parser')
+  soup = get_parsed_html(url)
   cards = soup.select('div.list-rst__body')
   shops = int(soup.select('.c-page-count__num strong')[2].text)
   pages = math.ceil(shops / 20)
@@ -21,8 +44,7 @@ def test():
   for i in range(pages):
     if i >= 1:
       url = base_url + str(i+1) + query
-      html = requests.get(url).text
-      soup = BeautifulSoup(html, 'html.parser')
+      soup = get_parsed_html(url)
       cards = soup.select('div.list-rst__body')
 
 
@@ -54,14 +76,106 @@ def test():
       csv.append(l)
     
     print(i+1)
-    time.sleep(5)
+    time.sleep(5)           
 
-  with open('patisseries.csv', 'w', encoding='utf_8_sig') as f:
-    for l in csv:
-      f.write(','.join(l) + '\n')
+  # urlだけを抽出 全ての結果が欲しければ、この行はコメントアウト
+  csv = map(lambda x: [x[1]], csv) 
+  f_name = 'patisseries_url.csv'
+  create_csv(f_name, csv)
 
-  # print(soup.title)
+# urlのcsvから食べログの詳細ページ情報を取得する
+# params: void
+# return: void
+def get_individuals():
+  def _get_one(soup):
+    def _get_one_element(css):
+      el = soup.select_one(css)
+      # print(el)
+      el = el.text.strip() if el else '-'
+      return el
+    return _get_one_element
 
+  def _get_many(soup):
+    def _get_many_element(css):
+      els = soup.select(css)
+      # print(els)
+      els = [el.text.strip() for el in els]
+      return els
+    return _get_many_element
+  
+  csv = [[
+    'name',
+    'subname',
+    'url',
+    'holiday',
+    'nearest',
+    'genre',
+    'price_dinner',
+    'price_lunch',
+    'star',
+    'address',
+    'tel',
+    'reviews',
+    'bookmarks',
+    'menu_url',
+    'photo_out',
+    'photo_in',
+  ]]
+
+  with open('patisseries_url.csv', 'r', encoding='utf_8_sig') as f:
+    for url in f:
+      url = url.strip()
+      soup = get_parsed_html(url)
+      if not soup: continue 
+
+      _get_el = _get_one(soup)
+      _get_els = _get_many(soup)
+
+      name = _get_el('.display-name span')
+      subname = _get_el('span.alias')
+      holiday = _get_el('#short-comment')
+      nearest = _get_els('.linktree__parent-target-text')[0]
+      genre = ' '.join(_get_els('.linktree__parent-target-text')[2:])
+      price_dinner = _get_el('.rdheader-budget__icon--dinner a')
+      price_lunch = _get_el('.rdheader-budget__icon--lunch a')
+      star = _get_el('.rdheader-rating__score-val-dtl')
+      address = ' '.join(_get_els('.rstinfo-table__address span'))
+      tel = _get_el('rstdtl-side-yoyaku__tel-number')
+      reviews = _get_el('rdheader-rating__review-target em')
+      bookmarks = _get_el('rdheader-rating__hozon-target em')
+      menu_url = url + 'dtlmenu'
+      photo_out = url + 'dtlphotolst/3/smp2'
+      photo_in  = url + 'dtlphotolst/4/smp2'
+
+      l = [
+        name,
+        subname,
+        url,
+        holiday,
+        nearest,
+        genre,
+        price_dinner,
+        price_lunch,
+        star,
+        address,
+        tel,
+        reviews,
+        bookmarks,
+        menu_url,
+        photo_out,
+        photo_in,
+      ]
+      # ,を消去
+      l = list(map(lambda x: x.replace(',', ''), l))
+      print(f'{len(csv)}: {l[0]}, {l[1]}, {l[9]}')
+      csv.append(l)
+
+      time.sleep(5)
+  f.close()
+
+  f_name = 'patisseries_detail.csv'
+  create_csv(f_name, csv)
 
 if __name__ == "__main__":
-  test()
+  # get_patisserie_list()
+  get_individuals()
